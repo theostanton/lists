0a26091c3dfdb771e35009e2cbeaf12c8c5a7d96
theostanton <theostanton@gmail.com>
2016-08-25 10:29:53 +0100

    Add db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,6 +17,22 @@ app.get('/', function (request, response) {
 
 app.get('/cool', function (request, response) {
     response.send(cool());
+});
+
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        client.query('SELECT * FROM test_table', function (err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                response.send("Error " + err);
+            }
+            else {
+                response.render('pages/db', {results: result.rows});
+            }
+        });
+    });
 });
 
 app.get('/times', function (request, response) {
