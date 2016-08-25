191be63b670e6a35b7320d555a1a15a28a4bc13c
theostanton <theostanton@gmail.com>
2016-08-25 11:57:35 +0100

    Deploy

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
+var lists = require('./routes/lists');
+var spotify = require('./routes/spotify');
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -43,8 +47,17 @@ app.get('/times', function (request, response) {
     response.send(result);
 });
 
+app.get('/lists',lists.findAll);
+app.get('/lists/:id',lists.findById);
+
+
+app.get('/login',spotify.login);
+app.get('/spotify_callback',spotify.callback);
+
+
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
+
 
 
--- routes/spotify.js
+++ routes/spotify.js
@@ -1,3 +1,104 @@
 /**
  * Created by theostanton on 25/08/2016.
  */
+
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+
+var client_id = '92f1a296e55c470ab95ab7c0e5d123d6';
+var client_secret = '1571832b11cd44c7af02af2eefc12dff';
+// var redirect_uri = 'https://pacific-headland-98039.herokuapp.com/spotify_callback';
+var redirect_uri = 'localhost:5000/spotify_callback';
+
+var stateKey = 'spotify_auth_state';
+
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+exports.login = function(req, res) {
+
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // your application requests authorization
+    var scope = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+};
+
+
+exports.callback = function(req, res) {
+
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    } else {
+        res.clearCookie(stateKey);
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+        };
+
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+
+                var access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+
+                var options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                // use the access token to access the Spotify Web API
+                request.get(options, function(error, response, body) {
+                    console.log(body);
+                });
+
+                // we can also pass the token to the browser to make requests from there
+                res.redirect('/#' +
+                    querystring.stringify({
+                        access_token: access_token,
+                        refresh_token: refresh_token
+                    }));
+            } else {
+                res.redirect('/#' +
+                    querystring.stringify({
+                        error: 'invalid_token'
+                    }));
+            }
+        });
+    }
+};
