42b5b3497a88402beceaf9e725243035ba684868
theostanton <theostanton@gmail.com>
2016-08-25 11:40:32 +0100

    Deploy

---
+++ api/lists.js
@@ -0,0 +1,12 @@
+/**
+ * Created by theostanton on 25/08/2016.
+ */
+
+
+exports.findAll = function(req, res) {
+    res.send([{name:'wine1'}, {name:'wine2'}, {name:'wine3'}]);
+};
+
+exports.findById = function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
+};
